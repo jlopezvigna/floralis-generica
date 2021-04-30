fec6fea83cb580bfe8c39324a2cbb3a395aa2f6e
Julian <lopezvignajulian@gmail.com>
2021-04-29 22:15:56 -0300

    fix: fix

---
+++ _static/_next/static/PNb4P4cEiQoEGM5Pp5BhG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-032a26e110a6bffbd445.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-667955f719625dd9819a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
