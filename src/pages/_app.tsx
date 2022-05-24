7a3130ebcd65679ecd1f6162887d73d6f86e33ef
Julian <lopezvignajulian@gmail.com>
2022-05-24 11:42:59 -0300

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import '@fontsource/inter';
-
+import { AppProps } from 'next/app';
 import { setup } from 'twind';
+import Head from 'next/head';
 import twindConfig from '../twind.config';
 
 if (typeof window !== `undefined`) {
@@ -10,5 +10,23 @@ if (typeof window !== `undefined`) {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+        <script>
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
